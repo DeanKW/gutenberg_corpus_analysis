4c0fd6145c6d3f58b8c7b15565d1ec95c78bbf24
Dean Weiss <weiss.dean@gmail.com>
2025-03-12 23:25:07 -0400

    Add docstrings and make variable names more clear
    Other than adding docstrings, the only change is uncommenting download_from_metadata, removing
    an if statement, and renaming the outpath variable

--- download_books_from_metadata.py
+++ download_books_from_metadata.py
@@ -12,28 +12,61 @@ from gutenberg.src.metadataparser import make_df_metadata
 from gutenberg.src.bookshelves import get_bookshelves
 from gutenberg.src.bookshelves import parse_bookshelves
 
-def download_from_metadata(metadata_file, outpath):
+def download_from_metadata(metadata_file: str,  raw_outpath: str):
+    """Downloads books' raw text files from Project Gutenberg based on metadata file
+    Args:
+        metadata_file (str): Path to metadata csv
+        raw_outpath (str): Folder to save raw text files
+
+    Raises:
+        ValueError: If no valid ID column found in metadata file
+    """
     df = pd.read_csv(metadata_file)
     if 'id' in df.columns:
         df['id']=df['id'].str[2:]
         book_list = df['id'].tolist()
     if 'Text#' in df.columns:
         book_list = df['Text#'].tolist()
-    
-    for book_id in book_list:
-        download_book(book_id, outpath)
+    else:
+        raise ValueError('No valid ID column found in metadata file')
 
-def download_url(urlpath):
+    for book_id in book_list:
+        download_book(book_id, raw_outpath)
+
+def download_url(urlpath: str):
+    """Downloads a file from a url
+
+    Args:
+        urlpath (str): The file to download
+
+    Returns:
+        _type_: _description_
+    """
     try:
         # open a connection to the server
         with urlopen(urlpath, timeout=3) as connection:
             # read the contents of the html doc
             return connection.read()
     except:
+        # TODO: Make this specific
         # bad url, socket timeout, http forbidden, etc.
         return None
     
-def download_book(book_id, save_path):
+def download_book(book_id: int, save_path: str):
+    """Downloads a book from Project Gutenberg based on its ID
+
+    Saves the book as PG{book_id}_raw.txt in the save_path folder,
+    in order to match the Standard Project Gutenberg Corpus format.
+
+    Note that this skips the .mirror step
+
+    Args:
+        book_id (int): The ID of the book to be downloaded
+        save_path (str): the folder to save the book in
+
+    Returns:
+        str: Description of success or failure
+    """
     print(book_id)
     # construct the download url
     url = f'https://www.gutenberg.org/files/{book_id}/{book_id}-0.txt'
@@ -55,18 +88,19 @@ if __name__ == '__main__':
     
     parser.add_argument('--metadata', required=True, help='Path to the metadata CSV file')
     parser.add_argument('--outpath', default='dataset',
-                        help='Path to save the downloaded books')
+                        help='Path to gutenberg data folder')
     parser.add_argument('--skip_existing', default=False, help='Should existing books be skipped')
     args = parser.parse_args()
 
-    if args.outpath=='dataset':
-        outpath = os.path.join(args.outpath, 'raw')
-        os.makedirs(args.outpath, exist_ok=True)
     
-    #download_from_metadata(args.metadata, args.outpath)
+    raw_outpath = os.path.join(args.outpath, 'raw')
+    os.makedirs(raw_outpath, exist_ok=True)
+
+    download_from_metadata(args.metadata, raw_outpath)
 
     metadata_fold = os.path.join(args.outpath, 'metadata')
     os.makedirs(metadata_fold, exist_ok=True)
+
     # Bookshelves
     # -----------
     # Get bookshelves and their respective books and titles as dicts
