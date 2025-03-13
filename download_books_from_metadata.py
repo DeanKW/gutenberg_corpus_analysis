d147ab72a2381bf28e9cf629540157745530db05
Dean Weiss <weiss.dean@gmail.com>
2025-03-13 00:28:36 -0400

    Add docstrings and type hints

--- download_books_from_metadata.py
+++ download_books_from_metadata.py
@@ -1,9 +1,15 @@
+"""This module downloads of books from Project Gutenberg based on metadata files on all platforms.
+
+Unlike the original Standard Project Gutenberg Corpus, this script is platform agnostic, since
+it does not use rsync to download.  It's janky, but it works (I hope).
+"""
+
 import os, sys
-import pandas as pd
 from urllib.request import urlopen
 import argparse
 import pickle
 
+import pandas as pd
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
@@ -12,7 +18,8 @@ from gutenberg.src.metadataparser import make_df_metadata
 from gutenberg.src.bookshelves import get_bookshelves
 from gutenberg.src.bookshelves import parse_bookshelves
 
-def download_from_metadata(metadata_file: str,  raw_outpath: str):
+
+def download_from_metadata(metadata_file: str, raw_outpath: str):
     """Downloads books' raw text files from Project Gutenberg based on metadata file
     Args:
         metadata_file (str): Path to metadata csv
@@ -23,7 +30,7 @@ def download_from_metadata(metadata_file: str,  raw_outpath: str):
     """
     df = pd.read_csv(metadata_file)
     if 'id' in df.columns:
-        df['id']=df['id'].str[2:]
+        df['id'] = df['id'].str[2:]
         book_list = df['id'].tolist()
     if 'Text#' in df.columns:
         book_list = df['Text#'].tolist()
@@ -32,6 +39,7 @@ def download_from_metadata(metadata_file: str,  raw_outpath: str):
 
     for book_id in book_list:
         download_book(book_id, raw_outpath)
+
 
 def download_url(urlpath: str):
     """Downloads a file from a url
@@ -51,7 +59,8 @@ def download_url(urlpath: str):
         # TODO: Make this specific
         # bad url, socket timeout, http forbidden, etc.
         return None
-    
+
+
 def download_book(book_id: int, save_path: str):
     """Downloads a book from Project Gutenberg based on its ID
 
@@ -83,16 +92,15 @@ def download_book(book_id: int, save_path: str):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='download_book_from_metadata.py',
-                                     description='Download a book from Project Gutenberg')
-    
+    parser = argparse.ArgumentParser(
+        prog='download_book_from_metadata.py', description='Download a book from Project Gutenberg'
+    )
+
     parser.add_argument('--metadata', required=True, help='Path to the metadata CSV file')
-    parser.add_argument('--outpath', default='dataset',
-                        help='Path to gutenberg data folder')
+    parser.add_argument('--outpath', default='dataset', help='Path to gutenberg data folder')
     parser.add_argument('--skip_existing', default=False, help='Should existing books be skipped')
     args = parser.parse_args()
 
-    
     raw_outpath = os.path.join(args.outpath, 'raw')
     os.makedirs(raw_outpath, exist_ok=True)
 
